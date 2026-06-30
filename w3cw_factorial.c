d125bc68846f487edce6fb72a4c450ae65c1b2ee
Mehreen Shaikh <mehreen.shaikh.642@gmail.com>
2026-06-29 19:34:34 -0700

    Week 3: Algorithms

---
+++ w3cw_factorial.c
@@ -0,0 +1,27 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int factorial(int n);
+
+int main(void)
+{
+    // Get positive value for n
+    int n;
+    do
+    {
+        n = get_int("n: ");
+    }
+    while (n < 0);
+
+    // Print factorial
+    printf("%i\n", factorial(n));
+}
+
+int factorial(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    return n * factorial(n-1);
+}
